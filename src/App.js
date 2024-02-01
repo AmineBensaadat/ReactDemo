2ac86d64ab36d2c0f0d38967510791284ca4b8a7
bensaadat <bensaadat.amine@gmail.com>
2024-02-01 16:21:55 +0100

    add matial datatable

--- src/App.js
+++ src/App.js
@@ -3,36 +3,38 @@ import './App.css';
 import React, { useEffect, useState } from "react";
 import DataTable from 'react-data-table-component';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import ContactForm from './components/ContactForm';
-import MyDataTable from './components/datatable';
+import ContactForm from './components/ContactForm.js';
+import MyDataTable from './components/datatable.js';
+import CustomizedTables from './components/datatableMaterial.js';
 import axios from 'axios';
 
 function App() {
-  const [posts, setPosts] = useState([]);
+  // const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(response => {
-        setPosts(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios.get('https://jsonplaceholder.typicode.com/posts')
+  //     .then(response => {
+  //       setPosts(response.data);
+  //     })
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  // }, []);
 
-  return (
-    <ul>
-      {posts.map(post => (
-        <li key={post.id}>{post.title}</li>
-      ))}
-    </ul>
-  );
   // return (
-  // <div className='container mt-5'>
-  //   <MyDataTable />
-  //   <ContactForm />
-  // </div>
+  //   <ul>
+  //     {posts.map(post => (
+  //       <li key={post.id}>{post.title}</li>
+  //     ))}
+  //   </ul>
   // );
+  return (
+  <div className='container mt-5'>
+    < CustomizedTables />
+    <MyDataTable />
+    <ContactForm />
+  </div>
+  );
 }
 
 export default App;
